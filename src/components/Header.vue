190a036542d2ef283a31e6fb941d306b5cb4ab54
sampathb <sampatbingi@gmail.com>
2023-04-19 08:32:52 +0530

    alert modal

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -27,8 +27,8 @@
       </button>
     </div>
     <div class="flex">
-      <a
-        href="#"
+      <button
+        @click="$event => toggleAlertModal(true)"
         class="w-12 h-12 bg-[#E7F2F8] rounded-full flex items-center justify-center mr-5"
       >
         <svg
@@ -47,7 +47,7 @@
             fill="#FF0000"
           />
         </svg>
-      </a>
+      </button>
       <a
         href="#"
         class="w-12 h-12 bg-[#E7F2F8] rounded-full flex items-center justify-center mr-5"
@@ -67,14 +67,178 @@
       </a>
     </div>
   </div>
+  
+  <TransitionRoot appear :show="isAlertModalOpen" as="template">
+    <Dialog as="div" @close="$event => toggleAlertModal(false)" class="relative z-30">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-25"></div>
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div class="flex min-h-full items-center justify-center p-4 text-center">
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel class="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-body align-middle shadow-xl transition-all">
+              <div class="flex items-center">
+                <div class="w-full text-center p-6">
+                  <h3 class="w-full text-xl font-bold text-black mb-9">Alert</h3>
+                  <img src="../assets/images/cover/alert.png" class="block mx-auto mix-blend-multiply" />
+                  <p class="text-sm text-black my-5">
+                    Are you sure you want to assign this <br/> Employee to this object?
+                  </p>
+                  <div class="mt-5 flex justify-center">
+                    <button type="button" class="btn btn-white mr-5" @click="$event => toggleAlertModal(false)">No</button>
+                    <button type="button" class="btn btn-sky text-white" @click="$event => toggleAlertModal(false)">Yes</button>
+                  </div>
+                </div>
+                <div class="w-full max-w-sm bg-white p-6">
+                  <div class="flex items-center justify-between mb-5 ">
+                    <DialogTitle as="h3" class="text-xl font-bold text-black">Employee Objects</DialogTitle>
+                    <button @click="$event => toggleAlertModal(false)" class="w-7 h-7 bg-body rounded-md flex items-center justify-center">
+                      <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M7.42079 6.08931L11.7176 1.73259C12.1089 1.33579 12.1089 0.694406 11.7176 0.297603C11.3262 -0.0992008 10.6937 -0.0992008 10.3023 0.297603L6.00555 4.65432L1.70876 0.297603C1.31741 -0.0992008 0.684854 -0.0992008 0.293509 0.297603C-0.0978363 0.694406 -0.0978363 1.33579 0.293509 1.73259L4.5903 6.08931L0.293509 10.446C-0.0978363 10.8428 -0.0978363 11.4842 0.293509 11.881C0.488681 12.0789 0.744907 12.1784 1.00113 12.1784C1.25736 12.1784 1.51358 12.0789 1.70876 11.881L6.00555 7.52429L10.3023 11.881C10.4975 12.0789 10.7537 12.1784 11.01 12.1784C11.2662 12.1784 11.5224 12.0789 11.7176 11.881C12.1089 11.4842 12.1089 10.8428 11.7176 10.446L7.42079 6.08931Z"
+                          fill="#18203A"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                  <table class="w-full text-xs font-semibold text-left">
+                    <thead class="bg-body">
+                      <tr class="text-sm font-bold">
+                        <td class="p-4 rounded-l-xl">Object</td>
+                        <td class="p-4 rounded-r-xl">Project Name</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        v-for="project in Projects"
+                        :key="project.ID"
+                        class="border-b border-body"
+                      >
+                        <td class="text-xs p-4 font-bold">
+                          {{ project.ID }}
+                        </td>
+                        <td class="text-xs font-normal p-4">
+                          <div class="">
+                            <span class="block text-black font-bold">{{
+                              project.name
+                            }}</span>
+                            <span class="block text-[#8F9BB3] text-[10px]"
+                              >{{ project.open }} Open,
+                              {{ project.completed }} Completed</span
+                            >
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
     
-  <script>
+<script>
 // import { RouterLink } from "vue-router";
+import {ref} from "vue";
+import {TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle} from "@headlessui/vue";
 export default {
   name: "HeaderComponent",
   components: {
-    // RouterLink,
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogPanel,
+    DialogTitle 
+  },
+  setup() {
+    let isAlertModalOpen = ref(false);
+    let toggleAlertModal = (s) => {
+      isAlertModalOpen.value = s;
+    }
+    return {isAlertModalOpen, toggleAlertModal}
+  },
+  data() {
+    return {
+      Projects: [
+        {
+          ID: "1",
+          name: "Office Management",
+          open: 2,
+          completed: 10,
+          employee: 20,
+          date: "12-02-2021",
+          isCompleted: true,
+        },
+        {
+          ID: "2",
+          name: "Project Management",
+          open: 2,
+          completed: 10,
+          employee: 20,
+          date: "12-02-2021",
+          isCompleted: true,
+        },
+        {
+          ID: "3",
+          name: "Video Caling App",
+          open: 2,
+          completed: 10,
+          employee: 20,
+          date: "12-02-2021",
+          isCompleted: true,
+        },
+        {
+          ID: "4",
+          name: "Hospital Management",
+          open: 2,
+          completed: 10,
+          employee: 20,
+          date: "12-02-2021",
+          isCompleted: true,
+        },
+        {
+          ID: "5",
+          name: "Digital Marketing ",
+          open: 2,
+          completed: 10,
+          employee: 20,
+          date: "12-02-2021",
+          isCompleted: false,
+        },
+        {
+          ID: "6",
+          name: "Idea Management",
+          open: 2,
+          completed: 10,
+          employee: 20,
+          date: "12-02-2021",
+          isCompleted: false,
+        },
+      ],
+    };
   },
   props: {
     title: String,
